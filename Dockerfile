db1947e51cc89adb1ffd8bbff2ad7616de576f63
netobreba <jose.silvaneto@dce.ufpb.br>
2018-04-26 16:33:16 -0300

    ajuste na estrutura do projeto

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM node:latest
 
-COPY ../service /home
+COPY service /home/service
 WORKDIR /home/service/dist
 
 EXPOSE 9000
 
-CMD ["npm install"]
+RUN cd /home/service
+RUN npm install
 CMD ["node", "server.js"]
--- service/src/model/models.js
+++ service/src/model/models.js
@@ -1,11 +1,17 @@
 import Sequelize from 'sequelize'
 
 let sequelize = new Sequelize("sistemas_distribuidos", "sd", "postgres123",{
-    host: "database",
+    host: "localhost",
     port: "5432",
     dialect: "postgres",
     operatorsAliases: false
 })
+
+// let sequelize = new Sequelize('sistemas_distribuidos', null, null, {
+//     host: 'localhost',
+//     dialect: 'sqlite',
+//     storage: './data.sqlite'
+// });
 
 export let User =  sequelize.define("user", {
     name: Sequelize.STRING
